72361a372afa524122ba1ce3dcfe7f02f5cda7f4
Stanley Makazhe <eleclog@gmail.com>
2017-12-18 12:54:03 +0000

    Note to self End of first code block page 324

--- app/src/main/java/com/gamecodeschool/notetoself/MainActivity.java
+++ app/src/main/java/com/gamecodeschool/notetoself/MainActivity.java
@@ -1,11 +1,20 @@
 package com.gamecodeschool.notetoself;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,23 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Temporary code
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // Create a new DialogShowNote called dialog
-                DialogShowNote dialog = new DialogShowNote();
-
-                // Send the note via the sendNoteSelected method
-                dialog.sendNoteSelected(mTempNote);
-
-                // Create a dialog
-                dialog.show(getFragmentManager(), "123");
-            }
-        });
 
 
     }
@@ -57,6 +49,76 @@ public class MainActivity extends AppCompatActivity {
     public void createNewNote(Note n) {
         // Temporary code
         mTempNote = n;
+    }
+
+    public class NoteAdapter extends BaseAdapter {
+
+        List<Note> noteList = new ArrayList<Note>();
+
+        @Override
+        public int getCount() {
+            return noteList.size();
+        }
+
+        @Override
+        public Note getItem(int whichItem) {
+            return noteList.get(whichItem);
+        }
+
+        @Override
+        public long getItemId(int whichItem) {
+            return whichItem;
+        }
+
+        @Override
+        public View getView(int whichItem, View view, ViewGroup viewGroup)
+        {
+
+            // Implement this method next
+            // Has view been inflated already
+            if(view == null){
+
+                // If not, do so here
+                // First create a LayoutInflater
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                // Now instantiate view using inflater.inflate
+                // using the listitem layout
+                view = inflater.inflate(R.layout.listitem, viewGroup,false);
+                // The false parameter is necessary
+                // because of the way that we want to use listitem
+
+            }// End if
+
+            // Grab a reference to all our TextView and ImageView widgets
+            TextView txtTitle = (TextView) view.findViewById(R.id.txtTitle);
+            TextView txtDescription = (TextView) view.findViewById(R.id.txtDescription);
+            ImageView ivImportant = (ImageView) view.findViewById(R.id.imageViewImportant);
+            ImageView ivTodo = (ImageView) view.findViewById(R.id.imageViewTodo);
+            ImageView ivIdea = (ImageView) view.findViewById(R.id.imageViewIdea);
+
+            // Hide an ImageView widget that are not relevant
+            Note tempNote = noteList.get(whichItem);
+
+            if (!tempNote.isImportant()){
+                ivImportant.setVisibility(View.GONE);
+            }
+
+            if (!tempNote.isTodo()){
+                ivTodo.setVisibility(View.GONE);
+            }
+
+            if (!tempNote.isIdea()){
+                ivIdea.setVisibility(View.GONE);
+            }
+
+            // Add the text to the heading and description
+            txtTitle.setText(tempNote.getTitle());
+            txtDescription.setText(tempNote.getDescription());
+
+            return view;
+
+        }
     }
 }
 
